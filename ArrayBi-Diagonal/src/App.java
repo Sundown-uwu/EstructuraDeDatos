159c45c5d73de6514909545f0b73769d147c4918
Sundown-uwu <20243ds148@utez.edu.mx>
2025-10-10 08:57:06 -0600

    Hice una suma de un array sin la suma de la diagonal principal

---
+++ ArrayBi-Diagonal/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int[][] arrTest={
+            {1,3,7,8},
+            {0,6,3,1},
+            {1,2,3,4},
+            {0,5,3,0},
+            {2,5,9,0}
+        };
+        int suma = 0;
+        for(int i = 0; i<arrTest.length; i++){
+            for(int j = 0; j < arrTest[i].length; j++){
+                if(i != j){
+                    suma += arrTest[i][j];
+                }
+            }
+    }
+                System.out.println("Suma del arreglo sin diagonal principal fue: " + suma);
+
+}
+}
